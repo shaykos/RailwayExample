5c771e0314b623366a814423f819b023acc6f897
Shaykos <shaykos@gmail.com>
2023-05-17 11:53:13 +0300

    added h1 just for fun

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <h1>HI! I can change it now... </h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
